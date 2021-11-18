437fff830ce53c3a0ca9c0c36d299e1a703d75d3
Phil <philip.connah93@gmail.com>
2021-11-19 07:19:18 +1000

    updated root component

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,3 +1,3 @@
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  return <section style={{ color: 'white' }}>{props.name} is mounted from the main branch!</section>;
 }
